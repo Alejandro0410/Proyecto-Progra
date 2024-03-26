284a37ed0ac2bb7072638a35c8ed4d98bf8910a1
Alejandro Barquero <abarquero0550@ufide.ac.cr>
2024-03-26 11:51:13 -0600

    Update Usuario

--- FideMotor/src/main/java/com/mycompany/fidemotor/Usuario.java
+++ FideMotor/src/main/java/com/mycompany/fidemotor/Usuario.java
@@ -1,8 +1,77 @@
 package com.mycompany.fidemotor;
 
 public class Usuario {
-    String nombre, apellidos, id, telefono, contrasenha, direccion, sexo, fechaNacimiento, nacionalidad;
-    
-    
+    private String nombre; 
+    private String apellidos; 
+    private int id; 
+    private String telefono; 
+    private String contrasenha;
+    private String direccion; 
+    private String sexo;
+    private String fechaNacimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getContrasenha() {
+        return contrasenha;
+    }
+
+    public void setContrasenha(String contrasenha) {
+        this.contrasenha = contrasenha;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
     
 }
