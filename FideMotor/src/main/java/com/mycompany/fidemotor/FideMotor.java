d0ca71bf7b5f3465791cf2e3a844550a2e77edfb
Alejandro Barquero <abarquero0550@ufide.ac.cr>
2024-03-26 11:40:43 -0600

    Creacion del archivo del Proyecto

---
+++ FideMotor/src/main/java/com/mycompany/fidemotor/FideMotor.java
@@ -0,0 +1,8 @@
+package com.mycompany.fidemotor;
+
+public class FideMotor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ FideMotor/src/main/java/com/mycompany/fidemotor/Usuario.java
@@ -0,0 +1,8 @@
+package com.mycompany.fidemotor;
+
+public class Usuario {
+    String nombre, apellidos, id, telefono, contrasenha, direccion, sexo, fechaNacimiento, nacionalidad;
+    
+    
+    
+}
